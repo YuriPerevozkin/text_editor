8bd21aa2d47f5cceeb11edcd35cc778b83e448dd
YuriPerevozkin <perevozkinyuri@gmail.com>
2025-08-16 11:36:41 +0600

    refactor(cmd_line): functions

--- src/app/cmd_line.rs
+++ src/app/cmd_line.rs
@@ -1,10 +1,24 @@
+use std::fs;
 use crate::app::{App, Mode};
 
 pub fn execute(app: &mut App) {
     match app.cmd_buffer.as_str() {
-        "q" => app.alive = false,
-        "w" => app.editor.save_file().unwrap(),
+        "q" => quit(app),
+        "w" => write(app),
+        "wq" => {
+            write(app);
+            quit(app);
+        }
         _ => {}
     }
     app.switch_mode(Mode::Normal);
+}
+
+fn quit(app: &mut App) {
+    app.alive = false;
+}
+
+fn write(app: &mut App) {
+    fs::write(&app.editor.file, app.editor.buffer.join("\n")+"\n")
+        .expect("Coud not save file");
 }
