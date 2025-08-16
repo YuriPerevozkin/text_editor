346708783537bf5fc22a7d55fe2b5784581eefb6
YuriPerevozkin <perevozkinyuri@gmail.com>
2025-08-16 11:32:19 +0600

    refactor(ui): no more .clone()

--- src/ui.rs
+++ src/ui.rs
@@ -9,91 +9,72 @@ use crossterm::{
     cursor::SetCursorStyle,
 };
 use ratatui::{
-    layout::{
-        Alignment, Flex, Position
-    }, prelude::*, style::Stylize, symbols::border, text::Line, widgets::{
-        block::Title, Block, Paragraph, Widget
-    },
+    prelude::*, 
+    layout::{Flex, Position},
+    symbols::border,
+    widgets::{Block, Paragraph},
 };
 
 
-struct EditorWidget<'a> {
-    title: Title<'a>,
-    content: Vec<String>,
-}
+const LINE_NUMBER_OFFSET: usize = 5;
+const CURSOR_OFFSET: usize = LINE_NUMBER_OFFSET + 2;
 
-struct CommandLineWidget {
-    content: String
-}
 
 pub fn draw_app(app: &mut App) {
-    let editor_widget = EditorWidget {
-        title: Title::from(Line::from(app.editor.file.to_string().bold().blue())),
-        content: app.editor.buffer.clone(),
-    };
     set_cursor_style(app);
 
     app.terminal.draw(|f| {
-        f.render_widget(editor_widget, f.area());
+        render_editor(f.area(), f.buffer_mut(), &app.editor.buffer);
+
         f.set_cursor_position(Position::new(
-                <usize as TryInto<u16>>::try_into(app.editor.cursor.pos
-                    +len_of_int(app.editor.buffer.len())+2).unwrap(),
-                    <usize as TryInto<u16>>::try_into(app.editor.cursor.line+1).unwrap(),
+            (app.editor.cursor.pos + CURSOR_OFFSET) as u16,
+            (app.editor.cursor.line + 1) as u16,
         ));
+
         if app.mode == Mode::Command {
-            let command_line_widget = CommandLineWidget {
-                content: app.cmd_buffer.clone()
-            };
-            f.render_widget(command_line_widget, f.area());
+            render_command_line(f.area(), f.buffer_mut(), &app.cmd_buffer);
         }
     }).unwrap();
 }
 
+
+fn render_editor(area: Rect, buf: &mut Buffer, file_content: &Vec<String>) {
+    let block = Block::bordered()
+        .border_set(border::ROUNDED);
+
+    let mut content = String::new();
+
+    for (i, item) in file_content.iter().enumerate() {
+        let space_number = LINE_NUMBER_OFFSET - int_len(i + 1);
+        content = format!("{}{}{} {}\n", content, " ".repeat(space_number), i + 1, item)
+    }
+
+    Paragraph::new(content)
+        .block(block)
+        .render(area, buf);
+}
+
+fn render_command_line(area: Rect, buf: &mut Buffer, content: &String) {
+    let block = Block::new();
+
+    let [area] = Layout::vertical([Constraint::Length(1)])
+        .flex(Flex::End)
+        .areas(area);
+
+    Paragraph::new(format!(":{}", content))
+        .block(block)
+        .render(area, buf)
+}
+
 fn set_cursor_style(app: &mut App) {
     let cursor_style = match app.mode {
-        Mode::Normal => SetCursorStyle::SteadyBlock, _ => SetCursorStyle::SteadyBar,
+        Mode::Normal => SetCursorStyle::SteadyBlock,
+        _ => SetCursorStyle::SteadyBar,
     };
 
-    execute!(
-        stdout(),
-        cursor_style,
-    ).unwrap();
+    execute!(stdout(), cursor_style).unwrap();
 }
 
-impl Widget for EditorWidget<'_> {
-    fn render(self, area: Rect, buf: &mut Buffer) {
-        let block = Block::bordered()
-            .title(self.title.alignment(Alignment::Left))
-            .border_set(border::ROUNDED);
-
-        let mut content = String::new();
-
-        for (i, item) in self.content.iter().enumerate() {
-            let space_number = len_of_int(self.content.len()) - len_of_int(i+1);
-            content.push_str(format!("{}{} {}\n", " ".repeat(space_number), i+1, item).as_str())
-        }
-
-        Paragraph::new(content)
-            .block(block)
-            .render(area, buf);
-    }
-}
-
-impl Widget for CommandLineWidget {
-    fn render(self, area: Rect, buf: &mut Buffer) {
-        let block = Block::bordered()
-            .border_set(border::ROUNDED);
-
-        let [area] = Layout::vertical([Constraint::Length(3)])
-            .flex(Flex::End)
-            .areas(area);
-
-        Paragraph::new(format!(":{}",self.content))
-            .block(block)
-            .render(area, buf)
-    }
-}
-
-fn len_of_int(n: usize) -> usize {
-    n.to_string().len()
+fn int_len(n: usize) -> usize {
+    ((n as f32).log10() + 1.0) as usize
 }
